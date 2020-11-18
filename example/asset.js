42033a32a67db82551888aebf967f05fca8859bb
Andrew Johnson <ajzat25@gmail.com>
2020-11-18 01:45:46 -0500

    update asset example

--- example/asset.js
+++ example/asset.js
@@ -4,10 +4,10 @@ const Sb3 = require('../sb3.js');
 // create a project
 const project = new Sb3();
 
-const asset = project.sprite.asset(
+project.sprite.asset(
   __dirname + '/../default.png',
   'asset name',
-)
+);
 
 // write the file
 project.export(__dirname + '/asset.sb3');
